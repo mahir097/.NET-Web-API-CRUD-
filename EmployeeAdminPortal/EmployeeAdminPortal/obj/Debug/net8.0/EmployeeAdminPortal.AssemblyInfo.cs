d58056e2a9a9ee0d19b63ea055c89dc87c8695b4
Mahir <mahir.demir097@gmail.com>
2024-07-09 15:41:23 +0300

    Created Employee Model

--- EmployeeAdminPortal/EmployeeAdminPortal/obj/Debug/net8.0/EmployeeAdminPortal.AssemblyInfo.cs
+++ EmployeeAdminPortal/EmployeeAdminPortal/obj/Debug/net8.0/EmployeeAdminPortal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeAdminPortal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63e57550a2c42b92812e9377714bafcd0390f218")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeAdminPortal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeAdminPortal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
