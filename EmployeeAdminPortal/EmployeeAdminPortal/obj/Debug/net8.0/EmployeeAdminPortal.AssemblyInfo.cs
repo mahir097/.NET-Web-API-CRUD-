0814c7dd332255e93f0d7cdeccc5b90fe55da433
Mahir <mahir.demir097@gmail.com>
2024-07-10 11:25:50 +0300

    Created Controller

--- EmployeeAdminPortal/EmployeeAdminPortal/obj/Debug/net8.0/EmployeeAdminPortal.AssemblyInfo.cs
+++ EmployeeAdminPortal/EmployeeAdminPortal/obj/Debug/net8.0/EmployeeAdminPortal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeAdminPortal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92886047081ca97c4a26ad2fae9b563b63afd947")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+871189b67242898ec9875f50dd6686b5edc80f40")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeAdminPortal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeAdminPortal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
